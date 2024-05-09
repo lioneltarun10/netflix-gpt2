f9a9fb267432bc8801b8f46fde226023bc5ce3bd
lioneltarun10 <lioneltarun10@gmail.com>
2024-05-09 06:31:30 +0530

    Sign/Sign up form && Login /header page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
+import Body from "./components/Body.js";
 
-import './App.css';
 
 function App() {
-  return (
-    <div className="text-3xl font-bold text-green-800">
-       Namaste Everyone , let's build netflix gpt
-    </div>
-  );
+  return <Body />;
 }
 
 export default App;
