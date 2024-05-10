a271bff30a404507d58f50622c17a019fa0b7d1a
lioneltarun10 <lioneltarun10@gmail.com>
2024-05-10 06:22:51 +0530

    Sign In / Sign Up is setted up using firebase authentication docs

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,8 @@
 import { useRef, useState } from "react"
 import Header from "./Header"
 import { checkValidData } from "../utils/validate"
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../utils/firebase";
 
 
 const Login = () => {
@@ -24,6 +26,39 @@ const Login = () => {
     setErrorMessage(message)
 
     //SignIn/Signup
+    if(message) return;
+
+    //SignIn/Signup logic
+    if(!isSignInForm){
+      // signUp Logic
+      createUserWithEmailAndPassword(auth, email.current.value,password.current.value)
+      .then((userCredential) => {
+        // Signed up 
+        const user = userCredential.user;
+        console.log(user);
+        
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        setErrorMessage(errorCode + "-" + errorMessage);
+      });
+    }
+    else{
+      // Sign In Logic
+      signInWithEmailAndPassword(auth, email.current.value,password.current.value)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        setErrorMessage(errorCode + "-" + errorMessage)
+      });
+    }
+
   }
   
   const toggleSignInForm = () => {
